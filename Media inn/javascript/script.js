73b41658f170e903f7b9e83ebe81ba52050034ef
Dinand Nafzger <bosjesman2005@gmail.com>
2022-02-03 21:15:40 +0100

    d
    d

--- Media inn/javascript/script.js
+++ Media inn/javascript/script.js
@@ -10,7 +10,7 @@ function playButton() {
         monologue.play();
         music.play();
         music.volume = 0.075;
-        monologue.volume = 0.15;
+        monologue.volume = 0.75;
         intPlayButton = 1;
     }
     else {
