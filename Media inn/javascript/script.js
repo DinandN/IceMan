3b0d129dc42f62f01f038bb99ec0b9381264e07a
Dinand Nafzger <bosjesman2005@gmail.com>
2022-02-03 12:29:37 +0100

    qwe
    qwe

--- Media inn/javascript/script.js
+++ Media inn/javascript/script.js
@@ -1,3 +1,20 @@
+var intPlayButton = 0;
+
 function changeMusic(music) {
-    document.getElementById("audio").setAttribute('src', music);
+    document.getElementById("music").setAttribute('src', music);
+}
+function playButton() {
+    document.getElementById("monologue");
+    document.getElementById("music");
+    if (intPlayButton == 0) {
+        monologue.play();
+        music.play();
+        music.volume = 0.075;
+        intPlayButton = 1;
+    }
+    else {
+        monologue.pause();
+        music.pause();
+        intPlayButton = 0;
+    }
 }
